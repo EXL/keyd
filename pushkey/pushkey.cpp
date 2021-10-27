62d4a56ee5d8841a7299922de0b892b7e1702ece
EXL <exlmotodev@gmail.com>
2021-10-27 14:13:09 +0700

    [pushkey]: Trying to send uint16_t, drop key server pipe

--- pushkey/pushkey.cpp
+++ pushkey/pushkey.cpp
@@ -9,11 +9,12 @@
  *   Public Domain
  *
  * History:
+ *   27-Oct-2021: Trying to send uint16_t, drop key server pipe.
  *   26-Oct-2021: First draft POC prototype version.
  */
 
 // Motorola
-#include <moto_input/vkm_ext.h>
+//#include <moto_input/vkm_ext.h>
 
 // Linux
 #include <sys/ioctl.h>
@@ -30,9 +31,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define KEY_SERVER_PIPE "/tmp/ui-framework-keytest-fifo"
-
-typedef uint32_t KeyCode_t;
+typedef uint16_t KeyCode_t;
 
 static int print_error(const char *a_error) {
 	fprintf(stderr, a_error);
@@ -42,31 +41,23 @@ static int print_error(const char *a_error) {
 static int print_usage(void) {
 	fprintf(
 		stderr,
-		"|pushkey| by EXL, v1.0, 26-Oct-2021\n\n"
-		"Usage:\n\t"
-		"pushkey 0x80000001 (push 1)\n\t"
-		"pushkey 0x00000001 (release 1)\n\n\t"
-		"pushkey 0x80000001 server (push 1)\n\t"
-		"pushkey 0x00000001 server (release 1)\n"
+		"|pushkey| by EXL, v1.0, 27-Oct-2021\n\n"
+		"Usage:\n"
+		"\tpushkey i 0x8001 (push 1)\n"
+		"\tpushkey i 0x0001 (release 1)\n"
+		"\tpushkey b 0x8001 (push 1)\n"
+		"\tpushkey b 0x0001 (release 1)\n\n"
+		"\tpushkey i 0x31 ascii (push 1)\n"
+		"\tpushkey b 0x31 ascii (push 1)\n\n"
 	);
 	return 1;
 }
 
-static int send_event_to_server(const MOTO_INPUT_EVENT_T a_event) {
-	static int l_server_fd = -1;
-	if (l_server_fd == -1) {
-		l_server_fd = open(KEY_SERVER_PIPE, O_WRONLY);
-		if (l_server_fd < 0)
-			return print_error("pushkey: Error: Cannot connect to the \""KEY_SERVER_PIPE"\" server!\n");
-		return !(sizeof(a_event) == write(l_server_fd, &a_event, sizeof(a_event)));
-	}
-}
-
-static int send_event_to_driver(const MOTO_INPUT_EVENT_T a_event) {
-	const int l_keypad_dev = open(KEYPADI_DEVICENAME, O_RDWR);
+static int send_event_to_driver(const uint16_t a_keycode, const int a_ascii, const int a_keypad_b) {
+	const int l_keypad_dev = open((a_keypad_b) ? KEYPADB_DEVICENAME : KEYPADI_DEVICENAME, O_RDWR);
 	if (l_keypad_dev < 0)
 		return print_error("pushkey: Error: Cannot open keypad \""KEYPADI_DEVICENAME"\" device!\n");
-	if (ioctl(l_keypad_dev, KEYPAD_IOC_INSERT_EVENT, a_event) < 0) {
+	if (ioctl(l_keypad_dev, (a_ascii) ? KEYPAD_IOC_INSERT_ASCII_EVENT : KEYPAD_IOC_INSERT_EVENT, a_keycode) < 0) {
 		fprintf(stderr, "pushkey: Error: ioctl: %s\n", strerror(errno));
 		return print_error("pushkey: Error: Cannot insert event to the \""KEYPADI_DEVICENAME"\" device!\n");
 	}
@@ -74,19 +65,19 @@ static int send_event_to_driver(const MOTO_INPUT_EVENT_T a_event) {
 	return 0;
 }
 
-static int parse_keycode(const char *a_key, int a_server) {
-	KeyCode_t l_keycode = strtoll(a_key, NULL, 0);
-	fprintf(stderr, "Keycode is: 0x%08X\n", l_keycode);
+static int parse_keycode(const char *a_mode, const char *a_key, const int a_ascii) {
+	KeyCode_t l_keycode = strtoul(a_key, NULL, 0);
+	fprintf(stderr, "Keycode is: 0x%04X, dec: %hu\n", l_keycode, l_keycode);
 
-	MOTO_INPUT_EVENT_T l_event;
-	l_event.type = MOTO_INPUT_EVENT_TYPE_KEY;
-	l_event.event.key_event = l_keycode;
+//	MOTO_INPUT_EVENT_T l_event;
+//	l_event.type = MOTO_INPUT_EVENT_TYPE_KEY;
+//	l_event.event.key_event = l_keycode;
 
-	return (a_server) ? send_event_to_server(l_event) : send_event_to_driver(l_event);
+	return send_event_to_driver(l_keycode, a_ascii, (a_mode[0] == 'b'));
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 2)
+	if (argc < 3)
 		return print_usage();
-	return parse_keycode(argv[1], (argc > 2));
+	return parse_keycode(argv[1], argv[2], (argc > 3));
 }
